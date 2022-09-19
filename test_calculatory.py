e30560bfe9591b681e8d1b63d5b7057a7c48fc30
dp <dp@dps-MacBook-Pro.wired.duke.local>
2022-09-19 13:02:54 -0400

    add new test_file to test check_HDL

---
+++ test_calculatory.py
@@ -0,0 +1,11 @@
+from blood_calculator import *
+import pytest
+
+
+@pytest.mark.parametrize(
+    "input,expected",
+    [(65, "Normal"), (50, "Borderline Low"), (45, "Borderline Low"), (35, "Low")],
+)
+def test_check_HDL(input, expected):
+    answer = check_HDL(input)
+    assert answer == expected
